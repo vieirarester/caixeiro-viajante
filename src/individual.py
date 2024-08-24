1ed45c65a56a7b748acc3aef6d6c11b1131bc574
Ester Vieira <vieirarester@gmail.com>
2024-08-24 19:12:08 -0300

    adicionando funcoes na classe individual

--- src/individual.py
+++ src/individual.py
@@ -1,4 +1,5 @@
 import random
+import os
 from dataset_manager import load_data
 
 class Individual:
@@ -9,7 +10,7 @@ class Individual:
             self.genes = random.sample(range(58), 58)  # permutação das cidades
         else:
             self.genes = genes
-        self.fitness = 0.0
+        self.fitness = self.calculate_fitness(load_data(os.path.join('data', 'brazil58.xml')))
         assert all(0 <= gene < 58 for gene in self.genes), "Valores fora do intervalo de cidades"
 
     def calculate_fitness(self, cities):
@@ -40,7 +41,55 @@ class Individual:
 
         self.fitness = total_distance
         return self.fitness
+    
+    def fill_child(self, child, parent, end):
+        index = end + 1 # índice (gene) de onde vai começar a ser preenchido o indivíduo
+
+        # percorre todos os genes do pai
+        for gene in parent.genes:
+            if gene not in child:
+
+                # verifica se o índice chegou no final do indivíduo
+                if index >= 58:
+                    # se sim, o índice vai para o início da lista
+                    index = 0
+                # atribui o gene do pai à posição atual no filho
+                child[index] = gene
+                # move para a próxima posição (gene)
+                index += 1
+    
+    def crossover(self, other_parent):
+        # escolhe 2 pontos aleatórios entre 0 e 57
+        p1, p2 = sorted(random.sample(range(58), 2))
+        
+        child1 = [None] * 58
+        child2 = [None] * 58
+        
+        # copia os genes definidos entre o intervalo p1 e p2 do pai1 (self) para o filho1
+        child1[p1:p2+1] = self.genes[p1:p2+1]
+        # copia os genes definidos entre o intervalo p1 e p2 do pai2 (self) para o filho2
+        child2[p1:p2+1] = other_parent.genes[p1:p2+1]
+        
+        # preenche os genes restantes dos filhos
+        self.fill_child(child1, other_parent, p2)
+        self.fill_child(child2, self, p2)
+        
+        return Individual(child1), Individual(child2)
+    
+    def mutate(self):
+        # sorteia indíces para trocar
+        index1, index2 = random.sample(range(len(self.genes)), 2)
+        print(f"Os índices sorteados foram = {index1} e {index2}")
+
+        # troca os genes com índices sorteados
+        self.genes[index1], self.genes[index2] = self.genes[index2], self.genes[index1]
+
+        return self
 
     def __str__(self):
         return f'Individuo(genes = {self.genes}, fitness = {self.fitness:.2f})'
+    
+individual = Individual()
+print(f"Indivíduo original = {individual}")
 
+print(f"Indivíduo mutado = {individual.mutate()}")
